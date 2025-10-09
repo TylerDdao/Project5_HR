0240937b4411ecec7279dd367c8e1cf7171ab203
HuiYing00 <hhuang6593@conestogac.on.ca>
2025-10-09 07:25:33 -0400

    employeeinfo.h, employeeinfo.cpp and testcases

--- Project5_HR/Project5_HR/source.cpp
+++ Project5_HR/Project5_HR/source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "test.h"
+#include "employeeinfo.h"
 
 using namespace std;
 int add(int x, int y) {
@@ -7,6 +8,11 @@ int add(int x, int y) {
 }
 
 int main() {
-	cout << add(2, 6) << endl;
+	//cout << add(2, 6) << endl;
+
+	EmployeeInfoManagement manage;
+	manage.loadFromFile();
+	manage.management();
+	manage.saveToFile();
 	return 0;
 }
