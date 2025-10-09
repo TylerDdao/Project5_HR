d0188c5caf7e953009aac1095a14bc7d170a0d43
TylerDdao <baonamdao05@gmail.com>
2025-10-09 00:58:17 -0400

    Account class added

---
+++ Project5_HR/Project5_HR/account.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Account
+{
+public:
+	Account();
+	~Account();
+
+private:
+	int StaffID;
+
+};
+
+Account::Account()
+{
+}
+
+Account::~Account()
+{
+}
