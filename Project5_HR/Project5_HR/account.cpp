e7bc0ad04cd9fb792d044810daba037bf2b2bf09
TylerDdao <baonamdao05@gmail.com>
2025-10-09 23:03:50 -0400

    Add account class and implement its functions.

---
+++ Project5_HR/Project5_HR/account.cpp
@@ -0,0 +1,45 @@
+#include "account.h"
+
+Account::Account()
+{
+	this->staffID = NULL;
+	this->password = "";
+	this->accountType = "";
+}
+
+Account::~Account()
+{
+}
+
+bool Account::setStaffId(int staffId)
+{
+	this->staffID = staffId;
+	return true;
+}
+
+bool Account::setPassword(string password)
+{
+	this->password = password;
+	return true;
+}
+
+bool Account::setAccountType(string accountType)
+{
+	this->accountType = accountType;
+	return true;
+}
+
+int Account::getStaffId()
+{
+	return staffID;
+}
+
+string Account::getPassword()
+{
+	return password;
+}
+
+string Account::getAccountType()
+{
+	return accountType;
+}
