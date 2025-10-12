550aa762aa33ca923a15b02ffb5956626b3994e1
TylerDdao <baonamdao05@gmail.com>
2025-10-11 21:30:20 -0400

    Created home page

---
+++ Project5_HR/Project5_HR_Front_End/src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const HomePage = () => (
+  <div className="p-8">
+    <h1 className="text-3xl font-bold text-gray-800 mb-4">Home Page</h1>
+    <p className="text-gray-600">Welcome to our simple multi-page React application!</p>
+    <p className="mt-2">Click the "About" button in the navigation to see the other page.</p>
+  </div>
+);
+
+export default HomePage;
