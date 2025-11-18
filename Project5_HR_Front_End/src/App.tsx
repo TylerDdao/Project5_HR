af74b05b5cbbd904477872c61f3e408903ba0d11
TylerDdao <baonamdao05@gmail.com>
2025-11-17 22:28:32 -0500

    Move front end folder

---
+++ Project5_HR_Front_End/src/App.tsx
@@ -0,0 +1,68 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+import DesignPage from './pages/design';
+import DashboardPage from './pages/dashboard'; // Notice you don't need to write '/index.tsx'
+import LoginPage from './pages/login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SchedulePage from './pages/schedule';
+import HomePage from './pages/home';
+import ShiftListPage from './pages/schedule/shift-list';
+import CreateShiftPage from './pages/schedule/create-shift';
+import ShiftDetailPage from './pages/schedule/shift-detail';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="">
+        <main className="">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+              
+              <Route path="/schedule" element={<SchedulePage/>}/>
+              <Route path="/schedule/shift-list" element={<ShiftListPage/>}/>
+              <Route path="/schedule/create-shift" element={<CreateShiftPage/>}/>
+              <Route path="/schedule/:shiftId" element={<ShiftDetailPage/>}/>
+
+              <Route path="/login" element={<LoginPage />} />
+              <Route path='/design' element={<DesignPage/>}/>
+            </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
---
+++ Project5_HR_Front_End/src/components/navBar.tsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+import type { Account, Role, Staff } from "../data/type";
+import { staffs } from "../data/dummyData";
+
+const navLinks: Record<Role, { name: string; path: string }[]> = {
+  Manager: [
+    { name: "Dashboard", path: "/dashboard" },
+    { name: "Schedule", path: "/schedule" },
+    { name: "Payroll", path: "/payroll" },
+    { name: "Employee Management", path: "/employee_management" },
+    { name: "Communication", path: "/communication" },
+  ],
+  Employee: [
+    { name: "Dashboard", path: "/dashboard" },
+    { name: "Schedule", path: "/schedule" },
+    { name: "Payroll", path: "/payroll" },
+    { name: "Communication", path: "/communication" },
+  ],
+};
+
+const NavBar: React.FC = () => {
+    const [staff, setCurrentStaff] = useState<Staff>();
+    const [account, setCurrentAccount] = useState<Account>();
+    useEffect(()=>{
+        setCurrentStaff(staffs[0]);
+
+        const staffStr = sessionStorage.getItem("staff");
+        if (staffStr) {
+            const staff = JSON.parse(staffStr) as Staff;
+            setCurrentStaff(staff);
+        }
+        const accountStr = sessionStorage.getItem("account");
+        if(accountStr){
+            const account = JSON.parse(accountStr) as Account;
+            setCurrentAccount(account)
+        }
+    }, [])
+    
+    const role = (account?.role as Role) || "Employee";
+    const name = staff?.name || "Unknown";
+
+    
+  
+  
+    const links = navLinks[role];
+    const location = useLocation(); // ✅ get current URL path
+
+    const handleLogout = () => {
+        sessionStorage.clear();
+        window.location.href = "/";
+      };
+
+    return (
+        <div className="fixed left-0 top-0 w-[400px] h-screen bg-charcoal p-5 flex flex-col">
+        <div className="flex items-center justify-center">
+            <AssignmentIndIcon className="text-light_gray !text-[37.5px] mr-[10px]" />
+            <div className="text-light_gray">
+            <h3>{name}</h3>
+            <div className="label">Role: {role}</div>
+            </div>
+        </div>
+
+        <div className="border border-light_gray w-full my-[30px]"></div>
+
+        <div className="flex flex-col space-y-[10px] justify-center items-center">
+            {links.map((link) => {
+            const isActive = location.pathname.startsWith(link.path);
+            return (
+                <Link
+                key={link.path}
+                to={link.path}
+                className={`p-[10px] rounded-xl text-center w-fit min-w-[300px] transition 
+                    ${
+                    isActive
+                        ? "bg-accent_blue text-light_gray"
+                        : "bg-light_gray text-charcoal hover:bg-gray-300"
+                    }`}
+                >
+                <h3 className={` ${
+                    isActive
+                        ? "text-light_gray"
+                        : "text-charcoal"
+                    }`}>{link.name}</h3>
+                </Link>
+            );
+            })}
+        </div>
+
+        <div className="flex flex-col justify-end h-full space-y-[10px] items-center mt-auto">
+            <button
+              className='bg-light_gray w-full'
+            >
+              <h3 className='text-charcoal'>My Account</h3>
+            </button>
+
+            <button
+              className='bg-accent_blue w-full'
+              onClick={handleLogout}
+            >
+              <h3 className='text-light_gray'>Log Out</h3>
+            </button>
+        </div>
+        </div>
+    );
+};
+
+export default NavBar;
