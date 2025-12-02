b0e948df80c8c4d87e20f11647ab202d137328d4
TylerDdao <baonamdao05@gmail.com>
2025-12-02 13:19:20 -0500

    Update index.tsx

--- Project5_HR_Front_End/src/pages/home/index.tsx
+++ Project5_HR_Front_End/src/pages/home/index.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
       <div className='h-screen flex flex-col justify-center items-center'>
         <div className='w-fit'>
           <div className='mb-[50px] flex flex-col space-y-[10px]'>
-            <h1>Welcome to HR Management System (Test)</h1>
+            <h1>Welcome to HR Management System (Test integrating with IT)</h1>
             <h3>Please login to continue</h3>
             <button className='w-fit bg-accent_blue text-light_gray' onClick={()=>window.location.href="/login"}>Login</button>
           </div>
