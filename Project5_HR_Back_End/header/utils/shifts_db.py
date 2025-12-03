262fae980a76c68bf68c66beedbb31aa9b63b0d3
TylerDdao <baonamdao05@gmail.com>
2025-12-02 20:29:30 -0500

    doxygen comments

--- Project5_HR_Back_End/header/utils/shifts_db.py
+++ Project5_HR_Back_End/header/utils/shifts_db.py
@@ -2,28 +2,29 @@ from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure, ServerSelectionTimeoutError
 from typing import Optional
 from datetime import datetime, timezone, timedelta
+import random
 
 from header.core.shift import Shift
+from header.utils.staffs_db import StaffsDatabase
+
 class ShiftsDatabase:
-    
     """
-    
+
     \brief Handles MongoDB operation for shifts collection.
         This class is responsible for 
         -> connecting to MOngoDB instance 
         -> performing read and update operations on shift documents
         in the shifts collection within database.
-    
+
     """
-    
+
     def __init__(self, uri="mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.9", database_name: str = "project5_hr"):
-       
         """
-       
-       \brief Construtor for ShiftsDB.
-       \param uri MongoDB connection URI.
-       \param database_name -> name of the db that stores shifts collection.
-       
+    
+        \brief Construtor for ShiftsDB.
+        \param uri MongoDB connection URI.
+        \param database_name -> name of the db that stores shifts collection.
+        
         """
         self._uri = uri
         self._database_name = database_name
@@ -32,7 +33,6 @@ class ShiftsDatabase:
         self._shifts = None
 
     def connect(self) -> Optional[Exception]:
-        
         """
         
         \brief Establish connection to MongoDB
@@ -41,7 +41,6 @@ class ShiftsDatabase:
         \return None if connection is successful, else raised Exception.
         
         """
-        
         try:
             self._client = MongoClient(self._uri)
             self._client.admin.command("ping")
@@ -54,9 +53,30 @@ class ShiftsDatabase:
         except Exception as err:
             print(f"Unexpected error: {err}")
             return err
+        
+    def get_shift_by_id(self, id: int) -> Optional[dict]:
+        if self._client is None or self._database is None:
+            error = self.connect()
+            if error:
+                return None
+
+        try:
+            shift = self._shifts.find_one({"shift_id": int(id)})
+            if not shift:
+                return None   # FIXED
+
+            staffDb = StaffsDatabase()
+            staff_docs = staffDb.get_staffs_by_ids(shift.get("staffs", []))
+            print(staff_docs)
+            shift["staffs"] = staff_docs
+            shift["_id"] = str(shift["_id"])
+            return shift
+
+        except Exception as e:
+            print("Error loading shift:", e)
+            return None
 
     def get_active_shifts(self) -> Optional[list[dict]]:
-        
         """
         
         \brief Get all current active shifts.
@@ -74,7 +94,7 @@ class ShiftsDatabase:
                 return None
 
         try:
-            now = datetime.now(timezone.utc)  # timezone-aware UTC
+            now = datetime.now(timezone.utc)
             query = {
                 "$and":[
                     {"start_time": {"$lte": now}},
@@ -82,22 +102,21 @@ class ShiftsDatabase:
                 ]
             }
 
-            shifts = list(self._shifts.find(query))
+            shifts = list(self._shifts.find(query).sort("start_time", 1))
 
             # Convert ObjectId to string for JSON
             for shift in shifts:
                 shift["_id"] = str(shift["_id"])
-
             return shifts
-
+                
+                
         except Exception as e:
             print(f"Error querying shifts: {e}")
             return None
 
     def get_scheduled_shifts(self) -> Optional[list[dict]]:
-        
         """
-        
+    
         \brief Get all shifts scheduled for future.
             Shift considered scheduled if
             -> start_time -> current UTC time
@@ -107,7 +126,6 @@ class ShiftsDatabase:
             or None if error occur or database cannot be reached.
         
         """
-        
         if self._client is None or self._database is None:
             error = self.connect()
             if error:
@@ -119,7 +137,7 @@ class ShiftsDatabase:
                 "start_time": {"$gt": now}
             }
 
-            shifts = list(self._shifts.find(query))
+            shifts = list(self._shifts.find(query).sort("start_time", 1))
 
             # Convert ObjectId to string for JSON
             for shift in shifts:
@@ -132,9 +150,8 @@ class ShiftsDatabase:
             return None
         
     def get_assigned_shifts(self, staff_id: int) -> Optional[list[dict]]:
-        
         """
-        
+            
         \brief Get shifts assigned to given staff member for this week and next week.
             Calculates the start and end of the current week and next week (in UTC)
             -> return all shifts assigned to given staff_id in those time ranges .
@@ -146,8 +163,7 @@ class ShiftsDatabase:
             All documents have objectid converted to string
             returns None if error occur or database cannot be reached.
         
-        """
-        
+        """ 
         if self._client is None or self._database is None:
             error = self.connect()
             if error:
@@ -155,16 +171,17 @@ class ShiftsDatabase:
         try:
             # Get this week
             now = datetime.now(timezone.utc)
+
             start_of_this_week = now - timedelta(days=now.weekday())
             start_of_this_week = start_of_this_week.replace(hour=0, minute=0, second=0)
             end_of_this_week = start_of_this_week + timedelta(days=6, hours=23, minutes=59, seconds=59)
 
             query = {
-                "staffs": staff_id,
+                "staffs": int(staff_id),
                 "start_time": {"$gte": start_of_this_week, "$lte": end_of_this_week}
             }
 
-            this_week_shifts = list(self._shifts.find(query))
+            this_week_shifts = list(self._shifts.find(query).sort("start_time", 1))
             for shift in this_week_shifts:
                 shift["_id"] = str(shift["_id"])
 
@@ -178,11 +195,12 @@ class ShiftsDatabase:
                 "start_time": {"$gte": start_of_next_week, "$lte": end_of_next_week}
             }
 
-            next_week_shifts = list(self._shifts.find(query))
+            next_week_shifts = list(self._shifts.find(query).sort("start_time", 1))
             for shift in next_week_shifts:
                 shift["_id"] = str(shift["_id"])
 
             shifts = {
+                # "active_shifts": active_shifts,
                 "this_week_shifts": this_week_shifts,
                 "next_week_shifts": next_week_shifts
             }
@@ -193,8 +211,32 @@ class ShiftsDatabase:
             print(f"Error querying shifts: {e}")
             return None
         
-    def update_shift(self, shift: Shift)->bool:
-        
+    def get_done_shifts(self) -> Optional[list[dict]]:
+        try:
+            self.connect()  # Ensure Mongo connection
+
+            # Query: end_time < now()
+            now = datetime.now()
+            cursor = self._shifts.find(
+                {"end_time": {"$lt": now}},
+                {"_id": 0, "shift_id": 1, "start_time": 1, "end_time": 1}
+            ).sort("start_time", 1).limit(1000)
+
+            shifts = []
+            for doc in cursor:
+                shifts.append({
+                    "shift_id": doc["shift_id"],
+                    "start_time": doc["start_time"].isoformat(),
+                    "end_time": doc["end_time"].isoformat()
+                })
+
+            return shifts if shifts else None
+
+        except Exception as e:
+            print(f"Error retrieving shifts: {e}")
+            return None
+
+    def update_shift(self, shift: Shift) -> bool:
         """
         \brief Update start and end time of existing shift.
             Find shift document by shift_id and update start time and end time using values from provided shift object.
@@ -204,24 +246,68 @@ class ShiftsDatabase:
         \return True if existing shift document was modified, False if no document was update or None if exception occur.
         
         """
-        
         if self._client is None or self._database is None:
             error = self.connect()
             if error:
-                return None
+                return False
 
         try:
-            original_shift = {
-                "shift_id": shift.get_shift_id()
+            query = {"shift_id": int(shift.get_shift_id())}
+            # found = self._shifts.find_one(query)
+            # print(found)
+            update_data = {
+                "$set": {
+                    "start_time": datetime.fromisoformat(shift.get_start_time()),
+                    "end_time": datetime.fromisoformat(shift.get_end_time()),
+                    "staffs": shift.get_staffs()
+                }
             }
-            updated_shift = {'$set':
-                {"start_time": datetime.fromisoformat(shift.get_start_time().replace("Z", "+00:00")),
-                "end_time": datetime.fromisoformat(shift.get_end_time().replace("Z", "+00:00"))}
-            }
-            result = self._shifts.update_one(original_shift, updated_shift)
 
-            return result
+            result = self._shifts.update_one(query, update_data)
+
+            # RETURN TRUE ONLY IF SOMETHING WAS UPDATED
+            return result.modified_count > 0
 
         except Exception as e:
-            print(f"Error querying shifts: {e}")
+            print(f"Error updating shift: {e}")
+            return False
+        
+    def insert_shift(self, shift: Shift) -> Optional[str]:
+        if self._client is None or self._database is None:
+            error = self.connect()
+            if error:
+                return False
+        try:
+
+            id = random.randint(10000, 99999)
+            while self.get_shift_by_id(id) is not None:
+                id = random.randint(10000, 99999)
+
+            doc = {
+                "shift_id": id,
+                "staffs": shift.get_staffs() or [],
+                "start_time": datetime.fromisoformat(shift.get_start_time()),
+                "end_time": datetime.fromisoformat(shift.get_end_time()),
+            }
+
+            self._shifts.insert_one(doc)
+            # Return stringified ObjectId
+            return id
+
+        except Exception as e:
+            print(f"Error inserting shift: {e}")
             return None
+        
+    def delete_shift(self, shift_id: int) -> bool:
+        if self._client is None or self._database is None:
+            error = self.connect()
+            if error:
+                return False
+        try:
+            # Convert to int if your staff_id is stored as integer
+            shift_id = int(shift_id)
+            result = self._shifts.delete_one({"shift_id": shift_id})
+            return result.deleted_count > 0
+        except Exception as e:
+            print(f"Error deleting shift: {e}")
+            return False
--- Project5_HR_Back_End/header/utils/staffs_db.py
+++ Project5_HR_Back_End/header/utils/staffs_db.py
@@ -1,18 +1,14 @@
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure, ServerSelectionTimeoutError
 from typing import Optional
+import random
+from datetime import datetime, timezone, timedelta
+
+
+from header.core.staff import Staff
 
 class StaffsDatabase:
-    
-    """
-    
-    \brief Handle MongoDB operations for staffs collection.
-    The class manage database connectivivty and provide functionality for staff authentication and staff record retrieval from staffs collection.
-    
-    """
-    
     def __init__(self, uri="mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.9", database_name: str = "project5_hr"):
-       
         """
         
         \brief Constructor for StaffsDatabse.
@@ -22,7 +18,6 @@ class StaffsDatabase:
         \param database_name Name of db that store staff collection.
         
         """
-       
         self._uri = uri
         self._database_name = database_name
         self._client = None
@@ -30,9 +25,7 @@ class StaffsDatabase:
         self._staffs = None
 
     def connect(self) -> Optional[Exception]:
-        
         """Connect to MongoDB and keep the client open."""
-        
         try:
             self._client = MongoClient(self._uri)
             self._client.admin.command("ping")
@@ -46,8 +39,7 @@ class StaffsDatabase:
             print(f"Unexpected error: {err}")
             return err
         
-    def verify_login(self, staff_id: str, password: str) -> Optional[dict]:
-        
+    def verify_login(self, staff_id: int, password: str) -> Optional[dict]:
         """
         
         \brief Verify staff login credentials.
@@ -63,29 +55,27 @@ class StaffsDatabase:
        \return Staff document dic if authentication succeed and None if credentials are invalid or if error occur.
        
         """
-        
         if self._client is None or self._database is None:
             error = self.connect()
             if error:
                 return None
         try:
             query = {
-                "$and":[
-                    {"staff_id": staff_id,},
-                    {"account.password" : password}
-                ]
+                "staff_id": int(staff_id),
+                "account.password": password
             }
             staff = self._staffs.find_one(query)
             if staff:
                 staff["_id"] = str(staff["_id"])
                 staff["hire_date"] = staff["hire_date"].isoformat()
+
+            print(staff)
             return staff
         except Exception as e:
             print(f"Error querying staff: {e}")
             return None
     
-    def get_staff(self, staff_id) -> Optional[dict]:
-        
+    def get_staff_by_id(self, staff_id) -> Optional[dict]:
         """
         
         \brief Retrieve staff record by staff ID.
@@ -99,21 +89,229 @@ class StaffsDatabase:
        \return Staff document dic if found and None if no match or if error occur.
        
         """
-        
         if self._client is None or self._database is None:
             error = self.connect()
             if error:
                 return None
         try:
             query = {
-                "staff_id": staff_id,
+                "staff_id": int(staff_id),
             }
             staff = self._staffs.find_one(query)
             if staff:
                 staff["_id"] = str(staff["_id"])  # make JSON serializable
                 staff["hire_date"] = staff["hire_date"].isoformat()
+
             return staff
         except Exception as e:
             print(f"Error querying staff: {e}")
             return None
+        
+    def get_staffs_by_ids(self, staff_ids: list[int]) -> list[dict]:
+        if self._client is None or self._database is None:
+            if self.connect():
+                return []
+
+        try:
+            cursor = self._staffs.find(
+                {"staff_id": {"$in": staff_ids}},
+                {"_id": 0, "staff_id": 1, "name": 1, "position": 1, "hire_date": 1}
+            )
+
+            staffs = []
+            for staff in cursor:
+                if "hire_date" in staff:
+                    staff["hire_date"] = staff["hire_date"].isoformat()
+                staffs.append(staff)
+
+            return staffs
+
+        except Exception as e:
+            print("Error querying staffs:", e)
+            return []
+        
+    def get_staffs(self, page: int) -> Optional[list[dict]]:
+        try:
+            self.connect()
+
+            skip = (page - 1) * 100
+            cursor = self._staffs.find({}, skip=skip, limit=100)
+
+            staffs = []
+            for r in cursor:
+                staffs.append({
+                    "staff_id": r.get("staff_id"),
+                    "name": r.get("name"),
+                    "position": r.get("position"),
+                    "phone_number": r.get("phone_number"),
+                    "hire_date": r.get("hire_date").isoformat() if r.get("hire_date") else None,
+                    "_id": str(r["_id"])
+                })
+
+            return staffs if staffs else None
+
+        except Exception as e:
+            print(f"Error retrieving staffs: {e}")
+            return None
+        
+    def set_clock_in(self, staff_id: int) -> bool:
+        if self._client is None or self._database is None:
+            error = self.connect()
+            if error:
+                return False
+        try:
+            query = {"staff_id": int(staff_id)}
+            found = self._staffs.find_one(query)
+            if found["is_working"] == True:
+                raise Exception("The staff already clocked in")
+
+            update_data = {
+                "$set": {
+                    "is_working": True
+                }
+            }
+
+            result = self._staffs.update_one(query, update_data)
+
+            # RETURN TRUE ONLY IF SOMETHING WAS UPDATED
+            return result.modified_count > 0
+
+        except Exception as e:
+            print(f"Error updating staffs: {e}")
+            return None
+        
+    def set_clock_out(self, staff_id: int) -> bool:
+        if self._client is None or self._database is None:
+            error = self.connect()
+            if error:
+                return False
+        try:
+            query = {"staff_id": int(staff_id)}
+            found = self._staffs.find_one(query)
+            if found["is_working"] == False:
+                raise Exception("The staff already clocked out")
+
+            update_data = {
+                "$set": {
+                    "is_working": False
+                }
+            }
+
+            result = self._staffs.update_one(query, update_data)
+
+            # RETURN TRUE ONLY IF SOMETHING WAS UPDATED
+            return result.modified_count > 0
+
+        except Exception as e:
+            print(f"Error updating staffs: {e}")
+            return None
+
+
+    def get_total_staffs(self) -> Optional[int]:
+        try:
+            self.connect()
+            return self._staffs.count_documents({})
+        except Exception as e:
+            print(f"Error retrieving total staffs: {e}")
+            return None
     
+    def insert_staff(self, staff: Staff) -> Optional[str]:
+        if self._client is None or self._database is None:
+            error = self.connect()
+            if error:
+                return False
+        try:
+
+            id = random.randint(10000, 99999)
+            while self.get_staff_by_id(id) is not None:
+                id = random.randint(10000, 99999)
+
+            doc = {
+                "staff_id": int(id),
+                "hire_date": datetime.fromisoformat(staff.get_hire_date()),
+                "phone_number": staff.get_phone_num(),
+                "position": staff.get_position(),
+                "name": staff.get_name(),
+                "wage_rate": float(staff.get_wage_rate()),
+                "account":{
+                    "account_type": staff.get_account_type(),
+                    "password": staff.get_password()
+                },
+                "is_working": False
+            }
+
+            self._staffs.insert_one(doc)
+            return id
+
+        except Exception as e:
+            print(f"Error inserting shift: {e}")
+            return None
+        
+    def update_staff(self, staff: Staff) -> bool:
+        if self._client is None or self._database is None:
+            error = self.connect()
+            if error:
+                return False
+        try:
+            query = {
+                "staff_id": int(staff.get_staff_id())
+            }
+
+            doc = {
+                "$set": {
+                    "hire_date": datetime.fromisoformat(staff.get_hire_date()),
+                    "phone_number": staff.get_phone_num(),
+                    "position": staff.get_position(),
+                    "name": staff.get_name(),
+                    "wage_rate": float(staff.get_wage_rate()),
+                    "account": {
+                        "account_type": staff.get_account_type(),
+                        "password": staff.get_password()
+                    },
+                    "is_working": False
+                }
+            }
+
+            result = self._staffs.update_one(query, doc)
+            return result.modified_count > 0
+
+        except Exception as e:
+            print(f"Error inserting shift: {e}")
+            return None
+        
+    def delete_staff(self, staff_id: int) -> bool:
+        if self._client is None or self._database is None:
+            error = self.connect()
+            if error:
+                return False
+        try:
+            # Convert to int if your staff_id is stored as integer
+            staff_id = int(staff_id)
+            result = self._staffs.delete_one({"staff_id": staff_id})
+            return result.deleted_count > 0
+        except Exception as e:
+            print(f"Error deleting staff: {e}")
+            return False
+        
+    def update_password(self, staff_id:int, old_password: str, new_password:str):
+        if self._client is None or self._database is None:
+            error = self.connect()
+            if error:
+                return False
+        try:
+            staff = self.verify_login(staff_id=int(staff_id), password=old_password)
+            if staff is None:
+                return False
+            query = {
+                "staff_id": staff_id
+            }
+            doc = {
+                "$set": {"account.password": new_password}
+            }
+
+            result = self._staffs.update_one(query, doc)
+            return result.modified_count > 0
+
+        except Exception as e:
+            print(f"Error inserting shift: {e}")
+            return None
